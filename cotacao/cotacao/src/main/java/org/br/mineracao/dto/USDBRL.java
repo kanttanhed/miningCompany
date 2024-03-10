47bb2c73db0dfbd026e810d68ff9f5294069378e
Plinio <plinio.salazar@yahoo.com.br>
2024-03-10 23:16:57 +0000

    Feat: Criação da entidade de cambio

---
+++ cotacao/cotacao/src/main/java/org/br/mineracao/dto/USDBRL.java
@@ -0,0 +1,27 @@
+package org.br.mineracao.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
+
+@Jacksonized
+@Data
+@Builder
+@AllArgsConstructor
+public class USDBRL {
+
+    private String code;
+    private String codein;
+    private String name;
+    private String high;
+    private String low;
+    private String varBid;
+    private String pctChange;
+    private String bid;
+    private String ask;
+    private String timestamp;
+    private String create_date;
+
+}
