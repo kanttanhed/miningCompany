d1b899b0530153663306dadbb42545106b0a2f7c
Plinio <plinio.salazar@yahoo.com.br>
2024-03-21 00:24:47 +0000

    Feat: Adição da anotação para converter para Json

--- cotacao/cotacao/src/main/java/org/br/mineracao/dto/USDBRL.java
+++ cotacao/cotacao/src/main/java/org/br/mineracao/dto/USDBRL.java
@@ -3,7 +3,6 @@ package org.br.mineracao.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
 
 @Jacksonized
