3908a9efc62a0027eae209c8f4e122b52522df1b
Plinio <plinio.salazar@yahoo.com.br>
2024-03-21 00:24:03 +0000

    Feat: Adição da anotação para o repositorio ser gerenciado pelo Quarkus

--- cotacao/cotacao/src/main/java/org/br/mineracao/repository/QuotationRepository.java
+++ cotacao/cotacao/src/main/java/org/br/mineracao/repository/QuotationRepository.java
@@ -1,7 +1,9 @@
 package org.br.mineracao.repository;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 import org.br.mineracao.entity.QuotationEntity;
 
+@ApplicationScoped
 public class QuotationRepository implements PanacheRepository<QuotationEntity> {
 }
